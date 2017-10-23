716cf3c09e4291b085d13ea864a35334edb16446
fa14-bse-053 <fa14-bse-053@LAB3D-04.cns.ciit>
2017-10-23 11:16:51 +0500

    Done

--- src/lab/webstore.java
+++ src/lab/webstore.java
@@ -5,11 +5,25 @@ import java.util.Map;
 
 public class webstore implements list{
 	 public String cart[] = {"A","A" , "B" ,"C" , "D"};	 int totle=0;
+	
 	 public webstore(){
 		 HashMap<String, Integer> itm = new HashMap<String, Integer>();
-			itm.put("A",3);
+			itm.put("A",0);
 			itm.put("B",1);
-			itm.put("C",1);  System.out.println("Item weight: " +itm.get("B"));
+			itm.put("C",2); // System.out.println("Item weight: " +itm.get("B"));
+			Integer ta=itm.get("A");
+			Integer tb=itm.get("B");
+			Integer tc=itm.get("C");
+			ta=ta+tb+tc;
+			System.out.println("Total Weight: "+ta);
+			double costt=0;
+			if(ta<=5){
+				costt=ta*0.75;
+			}
+			else 
+				costt = ta*0.50;
+			
+			System.out.println("Total costt: "+costt);
 	 }
 	 
 	 @Override
